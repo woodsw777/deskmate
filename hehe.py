fc3c9202c508b2295ce684d4c7edacd0608c83c3
woodsw777 <wjnwafu@163.com>
2019-09-24 15:37:23 +0800

    master修改

--- hehe.py
+++ hehe.py
@@ -1,8 +1,10 @@
-class Hello(object):
+lass Hello(object):
     send_guy = "孙一凡"
     def hehe(self):
         print("say hehe")
-
+    def eat(self):
+        print("今晚吃火锅")
 wangjian=Hello()
 wangjian.hehe()
+wangjian.eat()
 print(hello.send_guy)
